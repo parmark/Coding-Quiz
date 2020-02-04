d786d466728abdbc8afb8dabadaf5da8c43fadd6
Kevin Parmar <8l29.to@gmail.com>
2020-02-03 21:23:35 -0500

    Created a javascript, html, and css file with skeleton code and pseudo code.

---
+++ Assets/script.js
@@ -0,0 +1,25 @@
+
+// "View Highscores" link in the upper left
+// Time Counter in the upper right
+
+// Main Quiz Area
+// Elements are centered
+
+// Start Screen
+// Coding Quiz Header
+// Description of Quiz
+// Start Button
+
+// Ongoing Quiz
+// Question in large text
+// Clickable Answer Choices
+// After clicking immediately switch to next question
+// For 2 seconds show faded text below if correct or wrong
+
+//Ending Screen
+// Show score
+// Name input and enter button
+
+// High Score Screen
+// Shows a ul
+// Has a clear score option
